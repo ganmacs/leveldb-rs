7cb971d3c8efd6d5fe1290c89105a5cf5ba2adcb
ganmacs <ganmacs@gmail.com>
2018-05-05 00:57:13 +0900

    Impl TwoLevelIterator

--- src/version/compaction.rs
+++ src/version/compaction.rs
@@ -15,3 +15,56 @@ impl Compaction {
         }
     }
 }
+
+pub struct TwoLevelIterator<F, S> {
+    first: F,
+    second: Option<S>,
+}
+
+impl<F, S> TwoLevelIterator<F, S>
+where
+    F: Iterator<Item = S>,
+    S: Iterator,
+{
+    pub fn new(iter: F) -> Self {
+        TwoLevelIterator {
+            first: iter,
+            second: None,
+        }
+    }
+}
+
+impl<F, S> Iterator for TwoLevelIterator<F, S>
+where
+    F: Iterator<Item = S>,
+    S: Iterator,
+{
+    type Item = S::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.second.as_mut().and_then(|s| s.next()).or_else(|| {
+            self.first.next().and_then(|f| {
+                self.second = Some(f);
+                self.second.as_mut().and_then(|s| s.next())
+            })
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_two_level_iterator() {
+        let expecteds = (1..10).into_iter();
+        let v = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]
+            .into_iter()
+            .map(|v| v.into_iter());
+        let actuals = TwoLevelIterator::new(v.into_iter());
+
+        for (e, a) in expecteds.zip(actuals) {
+            assert_eq!(e, a);
+        }
+    }
+}
--- src/version/mod.rs
+++ src/version/mod.rs
@@ -7,7 +7,7 @@ mod compaction;
 pub use self::version_set::{Version, VersionSet};
 pub use self::version_edit::VersionEdit;
 pub use self::metadata::{FileMetaData, FileMetaDataBuilder};
-pub use self::compaction::Compaction;
+pub use self::compaction::{Compaction, TwoLevelIterator};
 use self::linked_list::CircularLinkedList;
 
 const BLOCK_SIZE: usize = 2 << 15; // duplicated
