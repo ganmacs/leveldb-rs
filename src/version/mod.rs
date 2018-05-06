add86e93a1a0ebef728f6cfa44d356481a01b281
ganmacs <ganmacs@gmail.com>
2018-05-07 00:09:51 +0900

    publish twoleveliterator

--- src/version/mod.rs
+++ src/version/mod.rs
@@ -7,7 +7,7 @@ mod compaction;
 pub use self::version_set::{Version, VersionSet};
 pub use self::version_edit::VersionEdit;
 pub use self::metadata::{FileMetaData, FileMetaDataBuilder};
-pub use self::compaction::{Compaction, TwoLevelIterator};
+pub use self::compaction::{Compaction, MergeingIterator, TwoLevelIterator};
 use self::linked_list::CircularLinkedList;
 
 const BLOCK_SIZE: usize = 2 << 15; // duplicated
